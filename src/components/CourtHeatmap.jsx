6a6ff014761ea45e10d66d147bde1e8c562b1b50
Max McCarty <max.r.mccarty@gmail.com>
2026-04-16 11:19:46 -0400

    COMP: Removed unused CourtHeatmap stub

--- src/components/CourtHeatmap.jsx
+++
@@ -1,4 +0,0 @@
-export default function CourtHeatmap({ data, onZoneClick }){
-    return <div className="aspect-[2/3] bg-orange-50 border rounded-xl flex items-center justify-center text-slate-500">Court SVG here</div>
-  }
-  
